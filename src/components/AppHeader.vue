cc2a0cc5cb2c528b06026e62b02e50a8960cfaa9
LorenzoPranio <lorenzopranio02@gmail.com>
2024-08-29 17:10:20 +0200

    [UPD]Added some elements in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,39 @@
 <script>
 export default {
-    
+
 }
 </script>
 
 <template>
-    <div>
-        
+    <div class="bg-head">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-6">
+                            <img src="../img/avadabarbers-logo-x2-200x70.png">
+                        </div>
+                        <div class="col-6">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                            <i class="fa-solid fa-bars"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+    .bg-head{
+        background-image: url('../img/avadabarbers-homepage-hero-bg.jpg');
+        height: 1000px;
+        background-size: cover;
+        
+        .fa-solid{
+            color: white;
+            margin: 5px;
+            font-size: x-large;
+        }
+    }
 </style>
