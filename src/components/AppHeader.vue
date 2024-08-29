d2a215c8e17792c888093f7ff4435bf309883f54
LorenzoPranio <lorenzopranio02@gmail.com>
2024-08-29 18:28:11 +0200

    [UPD]Added img in header and import font style for title in index.html

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,14 +10,24 @@ export default {
             <div class="row">
                 <div class="col-12">
                     <div class="row">
-                        <div class="col-6">
+                        <div class="col-6 pt-4">
                             <img src="../img/avadabarbers-logo-x2-200x70.png">
                         </div>
-                        <div class="col-6">
+                        <div class="col-6 text-end pt-4">
                             <i class="fa-solid fa-cart-shopping"></i>
                             <i class="fa-solid fa-bars"></i>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h1 class="abril-fatface-regular">Barber Shop</h1>
+                </div>
+                <div class="col-6">
+                    <img src="../img/avadabarbers_hero_focalmirror-600x825.png">
                 </div>
             </div>
         </div>
@@ -32,8 +42,16 @@ export default {
         
         .fa-solid{
             color: white;
-            margin: 5px;
+            margin: 10px;
             font-size: x-large;
         }
+        .abril-fatface-regular {
+            font-family: "Abril Fatface", serif;
+            font-weight: bolder;
+            font-style: normal;
+            color: white;
+            font-size: 90px;
+        }
+
     }
 </style>
