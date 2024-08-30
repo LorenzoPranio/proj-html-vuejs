ffecbf24c0da273d79e91060756e3d5c79ac4327
LorenzoPranio <lorenzopranio02@gmail.com>
2024-08-30 12:45:10 +0200

    [UPD]added static header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,9 +22,12 @@ export default {
             </div>
         </div>
         <div class="container">
-            <div class="row">
+            <div class="row align-items-center">
                 <div class="col-6">
                     <h1 class="abril-fatface-regular">Barber Shop</h1>
+                    <hr>
+                    <p>The Pinnacle of Male Grooming</p>
+                    <button><p>LEARN MORE</p></button>
                 </div>
                 <div class="col-6">
                     <img src="../img/avadabarbers_hero_focalmirror-600x825.png">
@@ -51,6 +54,32 @@ export default {
             font-style: normal;
             color: white;
             font-size: 90px;
+            margin-bottom: 30px;
+        }
+        .col-6{
+            hr{
+                color: #be9359;
+                border: 3px solid #be9359;
+                opacity: 1;
+                width: 270px;
+            }
+            p{
+                font-size: 25px;
+                color: #b0b0b0;
+                font-weight: 100;
+                margin: 40px 0px;
+            }
+            button{
+                border: 3px solid #be9359;
+                background-color: transparent;
+                padding: 10px 35px;
+                p{
+                    color: #be9359;
+                    font-size: 20px;
+                    font-weight: bold;
+                    margin: 0px;
+                }
+            }
         }
 
     }
