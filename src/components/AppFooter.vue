701b2b5a6292561581fba618133dfe23531580b1
LorenzoPranio <lorenzopranio02@gmail.com>
2024-08-31 16:31:18 +0200

    [UPD]Added footer section wothout style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,10 +6,58 @@ export default {
 
 <template>
     <div>
-        
+        <div class="bg-noir">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="row text-center">
+                            <div class="col-4">
+                                <img class="footer-img" src="../img/avadabarbers_footer_shop_image.png">
+                                <button><p>SHOP OUR RANGE NOW</p></button>
+                            </div>
+                            <div class="col-4">
+                                <img src="../img/avadabarbers-stickylogo-x2.png">
+                                <p>Avada Barbers <br>123 New York Street <br>New York City <br>info@yourwebsite.com <br>+1 (555) 555-1212</p>
+                                <p>FOLLOW US</p>
+                            </div>
+                            <div class="col-4">
+                                <img class="footer-img" src="../img/avadabarbers_footer_appointment_image.png">
+                                <button><p>BOOK YOUR APPOINTMENT</p></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-light-black">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <p>&copy Copyright 2012 - 2020 | Avada Theme by ThemeFusion | All Right Reserved | Powered by WordPress</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+    .bg-noir{
+        background-color: #121315;
+        .col-4{
+            margin-top: 60px;
+            .footer-img{
+                width: 450px;
+            }
+        }
+    }
+    .bg-light-black{
+        background-color: rgb(25, 25, 25);
+        p{
+            color: #b0b0b0;
+            font-weight: 100;
+            margin: 40px 0px;
+            font-size: 14px;
+        }
+    }
 </style>
